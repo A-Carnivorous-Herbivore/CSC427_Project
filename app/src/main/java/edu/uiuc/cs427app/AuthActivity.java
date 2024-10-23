5778329cc7ffe926939d879cfd447d36bdcd3564
zavaonah <rubenzneyroud@gmail.com>
2024-10-23 17:40:19 -0500

    initial commit to branch

---
+++ app/src/main/java/edu/uiuc/cs427app/AuthActivity.java
@@ -0,0 +1,20 @@
+package edu.uiuc.cs427app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import edu.uiuc.cs427app.databinding.ActivityMainBinding;
+
+import android.widget.Button;
+
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+}
+
