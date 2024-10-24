dbe6dbc23fe6f3afd643752987d76bdbc7f392d3
zavaonah <rubenzneyroud@gmail.com>
2024-10-23 19:02:29 -0500

    Mongo class file added

--- app/src/main/java/edu/uiuc/cs427app/AuthActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/AuthActivity.java
@@ -1,20 +1,42 @@
-package edu.uiuc.cs427app;
-
-import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
-import androidx.navigation.ui.AppBarConfiguration;
+import static com.mongodb.client.model.Filters.eq;
 
-import edu.uiuc.cs427app.databinding.ActivityMainBinding;
+public class AuthActivity extends AppCompatActivity {
+    private MongoDBClient mongoDBClient;
 
-import android.widget.Button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
 
-public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
+        // Initialize MongoDB Client
+        mongoDBClient = MongoDBClient.getInstance(this);
 
+        // Fetch user by username
+        fetchUserByUsername("exampleUser");
+    }
 
+    private void fetchUserByUsername(String username) {
+        MongoDatabase database = mongoDBClient.getMongoClient().getDatabase("AndroidApp");
+        MongoCollection<Document> usersCollection = database.getCollection("users");
+
+        // Find the user with the specified username
+        Document user = usersCollection.find(eq("username", username)).first();
+
+        if (user != null) {
+            // User found, show a toast or perform any action
+            String email = user.getString("email"); // Example: fetch the email
+            Toast.makeText(this, "User found: " + email, Toast.LENGTH_SHORT).show();
+        } else {
+            // User not found
+            Toast.makeText(this, "User not found.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
-
